1747654f6699a5e23345545ff7b8f1327c43a7cd
Krisztian Pocza <kpocza@kpocza.net>
2015-02-26 23:04:02 +0100

    Fix api urls for webite

--- Service/Web/IoT.Web/config/siteRoots.dev.js
+++ Service/Web/IoT.Web/config/siteRoots.dev.js
@@ -1,4 +1,4 @@
 ﻿app.constant('siteRoots', {
-    managementRoot: 'http://localhost/api/',
-    reportingRoot: 'http://localhost/rapi/'
+    managementRoot: 'http://localhost/api/v1',
+    reportingRoot: 'http://localhost/rapi/v1'
 });
--- Service/Web/IoT.Web/config/siteRoots.js
+++ Service/Web/IoT.Web/config/siteRoots.js
@@ -1,4 +1,4 @@
 ﻿app.constant('siteRoots', {
-    managementRoot: 'http://localhost:12345/api/',
-    reportingRoot: 'http://localhost:12345/rapi/'
+    managementRoot: 'http://localhost:12345/api/v1',
+    reportingRoot: 'http://localhost:12345/rapi/v1'
 });
