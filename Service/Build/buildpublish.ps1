7a408390c1111d07d908f21487b46be089a175f4
Krisztian Pocza <kpocza@kpocza.net>
2015-04-01 22:35:22 +0200

    Create installation artifacts in the build script

--- Service/Build/buildpublish.ps1
+++ Service/Build/buildpublish.ps1
@@ -5,6 +5,18 @@ param (
 	[string]$config = "ProdAzure"
 )
 
+function EnsureEmptyDirectory([string]$dirPath)
+{
+	if(test-path $dirPath) 
+	{
+		rm -Force -Recu $dirPath\*
+	}
+	else
+	{
+		mkdir $dirPath 
+	}
+}
+
 &$msbuild $solutionRoot\Iot.Service.sln /p:Configuration=$config /p:DebugSymbols=true
 
 &$msbuild $solutionRoot\Web\IoT.Web\IoT.Web.csproj /p:Configuration=$config /p:DeployOnBuild=true /p:AutoParameterizationWebConfigConnectionStrings=false /p:DeployTarget=Package /p:OutputPath=bin\$config /p:_PackageTempDir=$targetRoot\web /p:DebugSymbols=true
@@ -13,14 +25,7 @@ param (
 &$msbuild $solutionRoot\Messaging\IoT.Messaging.WebApi\IoT.Messaging.WebApi.csproj /p:Configuration=$config /p:DeployOnBuild=true /p:AutoParameterizationWebConfigConnectionStrings=false /p:DeployTarget=Package /p:OutputPath=bin\$config /p:_PackageTempDir=$targetRoot\msvc /p:DebugSymbols=true
 &$msbuild $solutionRoot\Reporting\IoT.Reporting.WebApi\IoT.Reporting.WebApi.csproj /p:Configuration=$config /p:DeployOnBuild=true /p:AutoParameterizationWebConfigConnectionStrings=false /p:DeployTarget=Package /p:OutputPath=bin\$config /p:_PackageTempDir=$targetRoot\rapi /p:DebugSymbols=true
 
-if(test-path $targetRoot\websocketservice) 
-{
-   rm -Force -Recu $targetRoot\websocketservice\*
-}
-else
-{
-   mkdir $targetRoot\websocketservice 
-}
+EnsureEmptyDirectory $targetRoot\websocketservice;
 
 cp -Recu -Force $solutionRoot\Platform\IoT.Platform.WebsocketService\bin\$config\* $targetRoot\websocketservice\
 
@@ -30,4 +35,22 @@ if($config -eq "DevAzure" -or $config -eq "DevSql") {
 
 if($config -eq "ProdAzure" -or $config -eq "ProdSql") {
 	rmdir -Recu -Force $targetRoot\web\config
+}
+
+EnsureEmptyDirectory $targetRoot\install;
+EnsureEmptyDirectory $targetRoot\install\configtemplates;
+EnsureEmptyDirectory $targetRoot\install\storage;
+EnsureEmptyDirectory $targetRoot\install\storage\messaging;
+EnsureEmptyDirectory $targetRoot\install\storage\management;
+
+if($config -eq "DevAzure" -or $config -eq "ProdAzure") {
+	cp $solutionRoot\Build\templates\config\azure\* $targetRoot\install\configtemplates
+	cp $solutionRoot\Messaging\Scripts\* $targetRoot\install\storage\messaging
+	cp -Recu $solutionRoot\Misc\IoT.CreateAzureStorage\bin\Debug\* $targetRoot\install\storage\management
+}
+
+if($config -eq "DevSql" -or $config -eq "ProdSql") {
+	cp $solutionRoot\Build\templates\config\sql\* $targetRoot\install\configtemplates
+	cp $solutionRoot\Messaging\Scripts\* $targetRoot\install\storage\messaging
+	cp -Recu $solutionRoot\Misc\IoT.CreateSqlStorage\bin\Debug\* $targetRoot\install\storage\management
 }
